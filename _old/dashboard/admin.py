8dfcab29166f36b8f0be24caf9e707aa4a751fe5
Lukasz Malucha <lucasmalucha@gmail.com>
2020-06-27 17:51:35 +0100

    post

--- _old/dashboard/admin.py
+++ _old/dashboard/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from dashboard.models import PropertyModel
 
+# Register your models here.
 class PropertyModelAdmin(admin.ModelAdmin):
     pass
 
-
-admin.site.register(PropertyModel,PropertyModelAdmin)
-
+admin.site.register(PropertyModel, PropertyModelAdmin)
 
--- _old/dashboard/models.py
+++ _old/dashboard/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 class PropertyModel(models.Model):
-    area=models.CharField(max_length=254)
+    location=models.CharField(max_length=254)
     property_type=models.CharField(max_length=254)
     bedrooms=models.IntegerField(default=1)
-    bathrooms = models.IntegerField(default=1)
-    price=models.DecimalField(max_digits=8,decimal_places=2)
+    bathrooms=models.IntegerField(default=1)
+    price=models.DecimalField(decimal_places=2, max_digits=12)
     sold=models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.area} property"
+        return f"Property: {self.location} {self.price}"
+
+
