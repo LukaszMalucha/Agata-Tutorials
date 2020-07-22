a3fb52a3efca661e8f855b20e7441e301cda9211
Lukasz Malucha <lucasmalucha@gmail.com>
2020-07-22 10:05:46 +0100

    ecommerce

--- _old/dashboard/models.py
+++ _old/dashboard/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 
 class PropertyModel(models.Model):
-    location=models.CharField(max_length=254)
+    area=models.CharField(max_length=254)
     property_type=models.CharField(max_length=254)
     bedrooms=models.IntegerField(default=1)
-    bathrooms=models.IntegerField(default=1)
-    price=models.DecimalField(decimal_places=2, max_digits=12)
+    bathrooms = models.IntegerField(default=1)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
     sold=models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Property: {self.location} {self.price}"
-
-
+        return f"{self.area} property"
--- dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import dashboard
-
-urlpatterns = [
-    path('main', dashboard, name="dashboard"),
-
-]
